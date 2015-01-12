27b4d847fbf33f3f3d913f57bc02b4b0da25b78b
Manish Johari <manish.johari@kiwitech.com>
2015-01-12 18:15:38 +0530

    Upload profile Image.
    Signed-off-by: Manish Johari <manish.johari@kiwitech.com>

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,8 +3,12 @@ class Api::V1::ProfilesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @profile = current_user.build_profile(create_params)
+    @profile = current_user.build_profile(create_profile_params)
     if @profile.save
+      # update and confirm email of user
+      current_user.email = create_email_params[:email]
+      current_user.save
+      current_user.confirm!
       render status: 201
     else
       render status: 422
@@ -13,7 +17,11 @@ class Api::V1::ProfilesController < ApplicationController
 
 private
 
-  def create_params
-    params.require(:profile).permit(:name, :status, :date_of_birth, :gender, profile_images_attributes: [:media])
+  def create_profile_params
+    params.require(:profile).permit(:name, :status, :date_of_birth, :gender, profile_image_attributes: [:media])
+  end
+
+  def create_email_params
+    params.require(:user).permit(:email)
   end
 end
